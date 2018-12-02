facb0296a7a14ce51c9b29e68898e7e678f5eeb7
Anatoly Trosinenko <anatoly.trosinenko@gmail.com>
2018-12-02 11:55:22 +0300

    Fix some warnings in generated helper wrappers

--- emscripten/gen_helper_wrappers.py
+++ emscripten/gen_helper_wrappers.py
@@ -12,19 +12,19 @@ typeDecl = (ZeroOrMore(delim + ident))('types')
 def_helper = Suppress('DEF_HELPER_FLAGS_' + Word(nums) + '(') + helper_name + delim + flags + typeDecl + Suppress(')' + ZeroOrMore(' '))
 
 sizes = {
-    'int': 1,
-    'env': 1,
-    'i32': 1,
-    'i64': 2,
-    's32': 1,
-    's64': 2,
-    'f32': 1,
-    'f64': 2,
-    'tl': 1,
-    'ptr': 1,
-    'MMXReg': 1,
-    'XMMReg': 1,
-    'ZMMReg': 1
+    'int': (1, 'int'),
+    'env': (1, 'void *'),
+    'i32': (1, 'uint32_t'),
+    'i64': (2, 'uint64_t'),
+    's32': (1, 'int32_t'),
+    's64': (2, 'int64_t'),
+    'f32': (1, 'float'),
+    'f64': (2, 'double'),
+    'tl' : (1, 'target_ulong'),
+    'ptr': (1, 'void *'),
+    'MMXReg': (1, 'void *'),
+    'XMMReg': (1, 'void *'),
+    'ZMMReg': (1, 'void *')
 }
 
 jumping_helpers = [] #["pause", "raise_interrupt", "raise_exception", "hlt"]
@@ -36,10 +36,11 @@ def gen_call(name, types):
     for t in types:
         if not first:
             res += ", "
-        res += "0"
-        sz = sizes[t]
+        res += "(" + sizes[t][1] + ")(0"
+        sz = sizes[t][0]
         for i in xrange(sz):
             res += " | (((long long)arg" + str(cur + i) + ") << " + str(i * 32) + ")"
+        res += ")"
         cur += sz
         first = False
     return res + ")"
@@ -49,7 +50,7 @@ print "#define WRAPPERS_H"
 for line in stdin:
     try:
         helper = def_helper.parseString(line)
-        print "long long " + helper.name + "_wrapper(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, arg11, arg12) {"
+        print "long long " + helper.name + "_wrapper(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12) {"
         if helper.types[0] in ["void", "noreturn"]:
             print "\t" + gen_call(helper.name, helper.types[1:]) + ";"
             print "\treturn 0;"
